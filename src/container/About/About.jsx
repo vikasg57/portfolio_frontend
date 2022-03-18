9046a0bf9f8b26e1f37a5f8449e183941380ed95
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-03-18 09:13:31 +0530

    about section fully modified

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,53 +1,117 @@
 
-import React from 'react'
-import {motion} from "framer-motion"
+  import React from 'react'
+import { motion } from 'framer-motion'
+
+import{AppWrap,MotionWrap} from "../../wrapper"
+
+ 
+
 import './About.scss'
-import {images} from '../../constants'
-import { urlFor,client } from "../../client";
-import { useEffect,useState } from 'react'
-import { AppWrap} from "../../wrapper";
-
-
 
 const About = () => {
 
-  const [abouts, setabouts] = useState([])
+   const experience=[
 
-  useEffect(() => {
+        {
+          year:'2021-2022',
+          works:[{ name:'Full-Stack Development',  college:"Masai School, Bangalore"}],
+        
+     
+        },
 
-    const query='*[_type=="abouts"]';
-
-    client.fetch(query).then((data)=>{ setabouts(data)   })
+        {
+          year:'2017-2019',
+          works:[{ name:'MBA(Operations)',college:"S.P.P.U,Pune"}]
+     
+        },
+          {
+          year:'2013-2017',
+          works:[{ name:'B. TECH(Food Tech)',college:"DR.B.S.K.K.V,Dapoli"}]
+     
+        },
   
-  }, [])
-  
+    ]
+
   return (
     <>
-    <h2 className='head-text'>I know That <span> Good Devlopment</span> <br /> means <span>Good apps</span></h2>
-    <div className='app__profiles'>
+      <h2 className='head-text'>
+        About me
 
-      {abouts.map((about,i)=>(
+      </h2>
+      <div className='app__about-des' >
+          <h3 className='p-text'  id='description'>Aspiring full-stack developer
+                                     with a specialization in MERN
+                                    stack. Possesses the ability to
+                                     understand business
+                                      requirements to translate them
+                                     into technical requirements. A
+                                       quick learner, eager to work in a
+                                      challenging environment, and
+                                    Proactive in time management.</h3>
 
-      
-       <motion.div
-       whileInView={{opacity:1}}
-       whileHover={{scale:1.1}}
-       transition={{duration:0.5,type:'tween'}}
-       className="app__profile-item"
-       key={about.title+i}
-       >
-        
-       <img src={urlFor(about.imgUrl)} alt={about.title}/>
-       <h2 className='bold-text' style={{marginTop:20}}> {about.title}</h2>
-       <h2 className='bold-text' style={{marginTop:10}}> {about.description}</h2>
+      </div>
+             
+               
+      <div
+      className='app__skills-container'>
+        <h2 className='head-text  app__exp'>
+        Education
 
-      </motion.div>))}
+      </h2> 
 
+           <motion.div
+           className='app__skills-exp'
+           >
+          
+             {experience?.map((experience)=>(
+               <motion.div
+               className='app__skills-exp-item'
+               key={experience.year}>
 
+                 <div
+                 className='app__skills-exp-year'
+                 >
+                   <p className='bold.text'>{experience.year}</p>
+                 </div>
+                 <motion.div className='app__skills-exp-works'>
 
-    </div>
+                {console.log(experience.works)}
+                   {experience.works.map((work)=>(
+               <>
+               <motion.div
+               className='app__skills-exp-work'
+                whileInView={{opacity:[0,1]}}
+                transition={{duration:0.5}}
+                data-tip
+                data-onFocusCapture={work.name}
+                key={work.name}
+            > 
+              <h4 className='bold-text'>{work.name}</h4>
+              <p className='p-text'>{work.college}</p>
+               </motion.div>
+
+               {/* <ReactTooltip 
+               effect="solid"
+               arrowColor="#fff"
+               className="skills-tooltip"
+               >
+                 {work.desc}
+               </ReactTooltip> */}
+               </>
+                   ))}
+                 </motion.div>
+               </motion.div>
+             ))}
+           </motion.div>
+
+      </div>
     </>
   )
 }
 
-export default AppWrap(About,"about")
+export default AppWrap(
+  MotionWrap(About,'app__skills'),
+
+  "about",
+  "app__whitebg"
+  )
